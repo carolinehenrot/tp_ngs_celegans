e87d4a7ee6841d7a4e06cebcb75db733dc2ea784
carolinehenrot <caroline.henrot@ens-lyon.fr>
2021-11-18 16:23:47 +0000

    rajout partie pour faire sur les données trimmées

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
 
-cd ~/mydatalocal/tp_ngs_celegans/data
-for SRR in "SRR5564855_1.fastq.gz SRR5564855_2.fastq.gz"
-do
-  fastqc $SRR --outdir ~/mydatalocal/tp_ngs_celegans/results
-done
+#cd ~/mydatalocal/tp_ngs_celegans/data
+#for SRR in "SRR5564866_1.fastq.gz SRR5564866_2.fastq.gz"
+#do
+ # fastqc $SRR --outdir ~/mydatalocal/tp_ngs_celegans/results
+#done
 
 #multiqsec 
 #cd ~/mydatalocal/tp_ngs_celegans/results
 #multiqsec results/
 
 
+#fastqc pour les données trimmées
+cd ~/mydatalocal/tp_ngs_celegans/results
+SRR_list="SRR5564855 SRR5564856 SRR5564857 SRR5564864 SRR5564865 SRR5564866"
+
+for file in ~/mydatalocal/tp_ngs_celegans/processed_data/trimming/*_paired.fq.gz
+do
+  fastqc $file --outdir ~/mydatalocal/tp_ngs_celegans/results/trimming_fastqc
+done

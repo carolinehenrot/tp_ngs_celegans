45f3464b052d907c8057e2d6f02821d9f7fb6eac
carolinehenrot <caroline.henrot@ens-lyon.fr>
2021-11-18 10:52:21 +0000

    maj

--- src/download_data.sh
+++ src/download_data.sh
@@ -2,9 +2,10 @@
 
 cd ~/mydatalocal/tp_ngs_celegans #je me positionne dans le dépôt mais de manière générale: plus maniable
 
-for SRR in "SRR5564855 SRR5564856 SRR5564857 SRR5564864 SRR5564865	SRR5564866"
+#SRR5564855
+for SRR in "SRR5564856 SRR5564857 SRR5564864 SRR5564865	SRR5564866"
 do 
-fastq-dump $SRR --split-3 --gzip --outdir data/ #la destination des données est précisée ici
+  parallel-fastq-dump --threads 6 -s $SRR --split-3 --gzip --outdir data/ #la destination des données est précisée ici
 done
 
 #zcat SRR5564855_1.fastq.gz |head -n10 pour afficher les premières séquences

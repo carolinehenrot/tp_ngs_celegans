40b8cb94115068372a34bc3134b677a26a4c623a
carolinehenrot <caroline.henrot@ens-lyon.fr>
2021-11-18 16:23:17 +0000

    ajout partie dézipper download genome

--- src/download_genome.sh
+++ src/download_genome.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
 cd ~/mydatalocal/tp_ngs_celegans/data
+wget http://ftp.ensembl.org/pub/release-104/fasta/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz #télécharge le fichier du lien
 
-wget http://ftp.ensembl.org/pub/release-104/fasta/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz
+#Pour dézipper le fichier téléchargé
+cd ~/mydatalocal/tp_ngs_celegans/data
+gunzip Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz
+

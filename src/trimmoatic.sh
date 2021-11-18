728c44d1e807af8798e4e28350472bbcd2dc1d37
carolinehenrot <caroline.henrot@ens-lyon.fr>
2021-11-18 16:22:37 +0000

    trimmomatic sur le transcriptome

--- src/trimmoatic.sh
+++ src/trimmoatic.sh
@@ -1,3 +1,24 @@
 #! /bin/bash
 
-java -jar trimmomatic-0.39.jar PE input_forward.fq.gz input_reverse.fq.gz output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:36
+#DATA CLEANING (TRIMMING)
+#maybe try several parameters, partition in the group
+cd ~/mydatalocal/tp_ngs_celegans/data
+mkdir -p trimming #ne râle pas si le fichier existe déjà
+cd trimming
+trimmomatic_path="/softwares/Trimmomatic-0.39/trimmomatic-0.39.jar"  #variable containing the path towards trimmomatic
+adapter_path="/softwares/Trimmomatic-0.39/adapters/TruSeq3-PE.fa"
+for file in ls ~/mydatalocal/tp_ngs_celegans/data/*_1.fastq.gz
+do #on ne veut garder que le nom du préfixe
+  prefix=${file/"_1.fastq.gz"/} #on enlève la fin du nom de file
+  prefix=${prefix/"/home/rstudio/mydatalocal/tp_ngs_celegans/data/"/} #et son début
+  java -jar $trimmomatic_path PE \
+  ~/mydatalocal/tp_ngs_celegans/data/${prefix}_1.fastq.gz \
+  ~/mydatalocal/tp_ngs_celegans/data/${prefix}_2.fastq.gz \
+  ${prefix}_forward_paired.fq.gz \
+  ${prefix}_forward_unpaired.fq.gz \
+  ${prefix}_reverse_paired.fq.gz \
+  ${prefix}_reverse_unpaired.fq.gz \
+  ILLUMINACLIP:${adapter_path}:2:30:10:2:keepBothReads \
+  LEADING:8 TRAILING:8 SLIDINGWINDOW:4:15 MINLEN:36
+done
+  
